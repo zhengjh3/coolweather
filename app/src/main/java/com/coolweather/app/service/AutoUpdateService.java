e85e549ffb871a72fe6e87b01059e7567f633ab3
zhengjh <zhengjh@gionee.com>
2017-07-14 14:47:42 +0800

    增加后台自动更新天气的功能
    Change-Id: Ib0a796780f0f4e0ed3a677602efe351e66322a44

---
+++ app/src/main/java/com/coolweather/app/service/AutoUpdateService.java
@@ -0,0 +1,65 @@
+package com.coolweather.app.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.coolweather.app.receiver.AutoUpdateReceiver;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+/**
+ * Created by root on 17-7-14.
+ */
+public class AutoUpdateService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeather();
+            }
+        }).start();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8 * 60 * 60 * 1000;
+        //Toast.makeText(this, "更新天气", Toast.LENGTH_SHORT).show();
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 更新天气信息。
+     */
+    private void updateWeather() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherCode = prefs.getString("weather_code", "");
+        String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                Utility.handleWeatherResponse(AutoUpdateService.this, response);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
